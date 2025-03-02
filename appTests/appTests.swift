59e20a809be297f9e916230c9a19764ea048ab19
Aksel <155363614+Aksel588@users.noreply.github.com>
2025-03-02 12:24:44 -0800

    Initial Commit

---
+++ appTests/appTests.swift
@@ -0,0 +1,17 @@
+//
+//  appTests.swift
+//  appTests
+//
+//  Created by Aksel on 02.03.25.
+//
+
+import Testing
+@testable import app
+
+struct appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
